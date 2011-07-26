8073e744c48a78f3df6656d81fe7dace7ffe43d0
Harr_cp <1819ba20a2256b002a7571681c51a783613d50e5asJx6J6n>
2011-07-26 19:58:50 +0000

    multiline subject fix
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS29]$/cyclopschat/trunk;C7833

---
+++ Cyclops.MainApplication/ValueConverters/SubjectConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Cyclops.MainApplication.ValueConverters
+{
+    public class SubjectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+            return value.ToString().Replace("\r\n", " | ").Replace("\n", " | ");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
