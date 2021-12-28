fb124c57d72544445dcfd72256d0414ce3216c71
Friedrich von Never <friedrich@fornever.me>
2021-12-28 23:07:48 +0700

    (#19) IMessage: code cleanup

--- Cyclops.Xmpp/Protocol/IMessage.cs
+++ Cyclops.Xmpp/Protocol/IMessage.cs
@@ -2,8 +2,8 @@ namespace Cyclops.Xmpp.Protocol;
 
 public interface IMessage : IStanza
 {
-    public string? Subject { get; }
-    public string? Body { get; }
+    string? Subject { get; }
+    string? Body { get; }
 
     IError? Error { get; }
 }
