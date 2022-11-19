6fe12e4bcb2520c0ca89982359b584ce04b96863
Friedrich von Never <friedrich@fornever.me>
2022-11-19 19:16:27 +0100

    LoginViewModel: formatting

--- Cyclops.MainApplication/ViewModel/LoginViewModel.cs
+++ Cyclops.MainApplication/ViewModel/LoginViewModel.cs
@@ -11,239 +11,238 @@ using Cyclops.MainApplication.Configuration;
 using Cyclops.Xmpp.Registration;
 using GalaSoft.MvvmLight.Command;
 
-namespace Cyclops.MainApplication.ViewModel
+namespace Cyclops.MainApplication.ViewModel;
+
+public class LoginViewModel : ViewModelBaseEx
 {
-    public class LoginViewModel : ViewModelBaseEx
+    private readonly ILogger logger;
+
+    private bool isBusy;
+    private string name;
+    private string server;
+    private string? errorMessage;
+    private IUserSession session;
+    private bool autoLogin;
+    private Profile profile;
+    private readonly IRegistrationManager registrationManager;
+    private string password;
+
+    private LoginViewModel(ILogger logger)
     {
-        private readonly ILogger logger;
+        this.logger = logger;
+        if (IsInDesignMode)
+            return;
 
-        private bool isBusy;
-        private string name;
-        private string server;
-        private string? errorMessage;
-        private IUserSession session;
-        private bool autoLogin;
-        private Profile profile;
-        private readonly IRegistrationManager registrationManager;
-        private string password;
+        profile = ApplicationContext.Current.CurrentProfile = ProfileManager.LoadProfile();
 
-        private LoginViewModel(ILogger logger)
+        registrationManager = ChatObjectFactory.GetRegistrationManager();
+        Server = profile.ConnectionConfig.Server;
+        Name = profile.ConnectionConfig.User;
+        AutoLogin = profile.AutoLogin;
+
+        Session = ChatObjectFactory.GetSession();
+        Session.AutoReconnect = false;
+        Session.Authenticated += SessionAuthenticated;
+        Session.ConnectionDropped += SessionConnectionDropped;
+        Authenticate = new RelayCommand<PasswordBox>(AuthenticateAction, AuthenticateCanExecute);
+        Register = new RelayCommand<PasswordBox>(RegisterAction, RegisterCanExecute);
+
+        if (AutoLogin)
         {
-            this.logger = logger;
-            if (IsInDesignMode)
-                return;
-
-            profile = ApplicationContext.Current.CurrentProfile = ProfileManager.LoadProfile();
-
-            registrationManager = ChatObjectFactory.GetRegistrationManager();
-            Server = profile.ConnectionConfig.Server;
-            Name = profile.ConnectionConfig.User;
-            AutoLogin = profile.AutoLogin;
-
-            Session = ChatObjectFactory.GetSession();
-            Session.AutoReconnect = false;
-            Session.Authenticated += SessionAuthenticated;
-            Session.ConnectionDropped += SessionConnectionDropped;
-            Authenticate = new RelayCommand<PasswordBox>(AuthenticateAction, AuthenticateCanExecute);
-            Register = new RelayCommand<PasswordBox>(RegisterAction, RegisterCanExecute);
-
-            if (AutoLogin)
+            try
             {
-                try
+                var decodedPassword = ChatObjectFactory.GetStringEncryptor().DecryptString(profile.ConnectionConfig.EncodedPassword);
+                AuthenticateAction(new PasswordBox { Password = decodedPassword });
+            }
+            catch
+            {
+                //TODO: log an exception
+            }
+        }
+    }
+
+    public LoginViewModel() : this(ChatObjectFactory.GetDebugLogger()) {}
+
+    public string ApplicationVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "N/A";
+
+    private bool RegisterCanExecute(PasswordBox obj)
+    {
+        return Validate();
+    }
+
+    private bool ValidatePassword(PasswordBox passwordBox)
+    {
+        ErrorMessage = string.Empty;
+        if (string.IsNullOrEmpty(passwordBox.Password))
+        {
+            ErrorMessage = Localization.Login.EmptyPassword;
+            return false;
+        }
+        return true;
+    }
+
+    private void RegisterAction(PasswordBox passwordBox)
+    {
+        if (!ValidatePassword(passwordBox))
+            return;
+
+        IsBusy = true;
+        password = passwordBox.Password;
+
+        DoRegister().NoAwait(logger);
+        async Task DoRegister()
+        {
+            RegistrationResult registrationResult;
+            try
+            {
+                registrationResult = await registrationManager.RegisterNewUser(new ConnectionConfig
                 {
-                    var decodedPassword = ChatObjectFactory.GetStringEncryptor().DecryptString(profile.ConnectionConfig.EncodedPassword);
-                    AuthenticateAction(new PasswordBox { Password = decodedPassword });
-                }
-                catch
-                {
-                    //TODO: log an exception
-                }
+                    Server = Server,
+                    User = Name,
+                    EncodedPassword = ChatObjectFactory.GetStringEncryptor().EncryptString(password),
+                });
             }
-        }
-
-        public LoginViewModel() : this(ChatObjectFactory.GetDebugLogger()) {}
-
-        public string ApplicationVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "N/A";
-
-        private bool RegisterCanExecute(PasswordBox obj)
-        {
-            return Validate();
-        }
-
-        private bool ValidatePassword(PasswordBox passwordBox)
-        {
-            ErrorMessage = string.Empty;
-            if (string.IsNullOrEmpty(passwordBox.Password))
+            catch (Exception ex)
             {
-                ErrorMessage = Localization.Login.EmptyPassword;
-                return false;
+                logger.LogError("Exception during registration.", ex);
+                registrationResult = new RegistrationResult(ex.Message);
             }
-            return true;
-        }
 
-        private void RegisterAction(PasswordBox passwordBox)
+            ErrorMessage = registrationResult.ErrorMessage;
+            if (!registrationResult.HasError)
+                AuthenticateAction(new PasswordBox {Password = password});
+            else
+                IsBusy = false;
+        }
+    }
+
+    public RelayCommand<PasswordBox> Authenticate { get; set; }
+    public RelayCommand<PasswordBox> Register { get; set; }
+
+    public string Name
+    {
+        get { return name; }
+        set
         {
-            if (!ValidatePassword(passwordBox))
-                return;
-
-            IsBusy = true;
-            password = passwordBox.Password;
-
-            DoRegister().NoAwait(logger);
-            async Task DoRegister()
-            {
-                RegistrationResult registrationResult;
-                try
-                {
-                    registrationResult = await registrationManager.RegisterNewUser(new ConnectionConfig
-                    {
-                        Server = Server,
-                        User = Name,
-                        EncodedPassword = ChatObjectFactory.GetStringEncryptor().EncryptString(password),
-                    });
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError("Exception during registration.", ex);
-                    registrationResult = new RegistrationResult(ex.Message);
-                }
-
-                ErrorMessage = registrationResult.ErrorMessage;
-                if (!registrationResult.HasError)
-                    AuthenticateAction(new PasswordBox {Password = password});
-                else
-                    IsBusy = false;
-            }
+            name = value;
+            RaisePropertyChanged("Name");
         }
+    }
 
-        public RelayCommand<PasswordBox> Authenticate { get; set; }
-        public RelayCommand<PasswordBox> Register { get; set; }
-
-        public string Name
+    public string Server
+    {
+        get { return server; }
+        set
         {
-            get { return name; }
-            set
-            {
-                name = value;
-                RaisePropertyChanged("Name");
-            }
+            server = value;
+            RaisePropertyChanged("Server");
         }
+    }
 
-        public string Server
+    public bool IsBusy
+    {
+        get { return isBusy; }
+        set
         {
-            get { return server; }
-            set
-            {
-                server = value;
-                RaisePropertyChanged("Server");
-            }
+            isBusy = value;
+            RaisePropertyChanged("IsBusy");
         }
+    }
 
-        public bool IsBusy
+    public bool AutoLogin
+    {
+        get { return autoLogin; }
+        set
         {
-            get { return isBusy; }
-            set
-            {
-                isBusy = value;
-                RaisePropertyChanged("IsBusy");
-            }
+            autoLogin = value;
+            RaisePropertyChanged("AutoLogin");
         }
+    }
 
-        public bool AutoLogin
+    public string? ErrorMessage
+    {
+        get { return errorMessage; }
+        set
         {
-            get { return autoLogin; }
-            set
-            {
-                autoLogin = value;
-                RaisePropertyChanged("AutoLogin");
-            }
+            errorMessage = value;
+            RaisePropertyChanged("ErrorMessage");
         }
+    }
 
-        public string? ErrorMessage
+    public IUserSession Session
+    {
+        get { return session; }
+        set
         {
-            get { return errorMessage; }
-            set
-            {
-                errorMessage = value;
-                RaisePropertyChanged("ErrorMessage");
-            }
+            session = value;
+            RaisePropertyChanged("Session");
         }
+    }
 
-        public IUserSession Session
+    public event EventHandler Authenticated = delegate { };
+
+    private void SessionConnectionDropped(object sender, AuthenticationEventArgs e)
+    {
+        IsBusy = false;
+        ErrorMessage = e.ErrorMessage;
+    }
+
+    private void SessionAuthenticated(object sender, AuthenticationEventArgs e)
+    {
+        if (e.Success)
         {
-            get { return session; }
-            set
-            {
-                session = value;
-                RaisePropertyChanged("Session");
-            }
+            Session.AutoReconnect = true;
+            //load smiles into context
+            ApplicationContext.Current.SmilePacks = ChatObjectFactory.GetSmilesManager().GetSmilePacks();
+            IsBusy = false;
+
+            if (ConfigurationManager.AppSettings["ShowXmppConsole"].StringToBool())
+                ChatObjectFactory.ShowDebugWindow();
+            Authenticated(this, e);
         }
-
-        public event EventHandler Authenticated = delegate { };
-
-        private void SessionConnectionDropped(object sender, AuthenticationEventArgs e)
+        else
         {
             IsBusy = false;
             ErrorMessage = e.ErrorMessage;
         }
+    }
 
-        private void SessionAuthenticated(object sender, AuthenticationEventArgs e)
+    private bool AuthenticateCanExecute(PasswordBox passwordBox)
+    {
+        return Validate();
+    }
+
+    private bool Validate()
+    {
+        return !string.IsNullOrWhiteSpace(Name) &&
+               !string.IsNullOrWhiteSpace(Server) &&
+               !Name.Contains("@") && !Name.Contains("/");
+    }
+
+    private void AuthenticateAction(PasswordBox passwordBox)
+    {
+        if (!ValidatePassword(passwordBox))
+            return;
+
+        IsBusy = true;
+        string encodedPsw = ChatObjectFactory.GetStringEncryptor().EncryptString(passwordBox.Password);
+        var connectionConfig = new ConnectionConfig
         {
-            if (e.Success)
-            {
-                Session.AutoReconnect = true;
-                //load smiles into context
-                ApplicationContext.Current.SmilePacks = ChatObjectFactory.GetSmilesManager().GetSmilePacks();
-                IsBusy = false;
+            EncodedPassword = encodedPsw,
+            Server = Server,
+            User = Name,
+            NetworkHost = profile.ConnectionConfig.NetworkHost,
+            Port = profile.ConnectionConfig.Port
+        };
+        profile.AutoLogin = AutoLogin;
+        profile.ConnectionConfig = connectionConfig;
+        ProfileManager.SaveProfile(profile);
+        Session.AuthenticateAsync(connectionConfig);
+    }
 
-                if (ConfigurationManager.AppSettings["ShowXmppConsole"].StringToBool())
-                    ChatObjectFactory.ShowDebugWindow();
-                Authenticated(this, e);
-            }
-            else
-            {
-                IsBusy = false;
-                ErrorMessage = e.ErrorMessage;
-            }
-        }
-
-        private bool AuthenticateCanExecute(PasswordBox passwordBox)
-        {
-            return Validate();
-        }
-
-        private bool Validate()
-        {
-            return !string.IsNullOrWhiteSpace(Name) &&
-                   !string.IsNullOrWhiteSpace(Server) &&
-                   !Name.Contains("@") && !Name.Contains("/");
-        }
-
-        private void AuthenticateAction(PasswordBox passwordBox)
-        {
-            if (!ValidatePassword(passwordBox))
-                return;
-
-            IsBusy = true;
-            string encodedPsw = ChatObjectFactory.GetStringEncryptor().EncryptString(passwordBox.Password);
-            var connectionConfig = new ConnectionConfig
-                                       {
-                                           EncodedPassword = encodedPsw,
-                                           Server = Server,
-                                           User = Name,
-                                           NetworkHost = profile.ConnectionConfig.NetworkHost,
-                                           Port = profile.ConnectionConfig.Port
-                                       };
-            profile.AutoLogin = AutoLogin;
-            profile.ConnectionConfig = connectionConfig;
-            ProfileManager.SaveProfile(profile);
-            Session.AuthenticateAsync(connectionConfig);
-        }
-
-        public override void Cleanup()
-        {
-            Session.Authenticated -= SessionAuthenticated;
-            Session.ConnectionDropped -= SessionConnectionDropped;
-        }
+    public override void Cleanup()
+    {
+        Session.Authenticated -= SessionAuthenticated;
+        Session.ConnectionDropped -= SessionConnectionDropped;
     }
 }
