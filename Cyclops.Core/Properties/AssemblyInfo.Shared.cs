79bf51a6a7eb0d201fd7128aad8c0d8125db3c20
Friedrich von Never <friedrich@fornever.me>
2022-11-19 19:19:51 +0100

    Release: prepare v2.1.0

--- Cyclops.Core/Properties/AssemblyInfo.Shared.cs
+++ Cyclops.Core/Properties/AssemblyInfo.Shared.cs
@@ -1,12 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-//SHARED!
-
 [assembly: AssemblyCompany("CyclopsChat Contributors")]
-[assembly: AssemblyCopyright("Copyright © CyclopsChat Contributors 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
