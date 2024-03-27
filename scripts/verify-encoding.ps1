b8247d1398f937ba3ea0757d71d3f507b524890c
Friedrich von Never <friedrich@fornever.me>
2024-03-28 00:47:24 +0100

    verify-encoding: filter out the .resx files
    They are generated by the IDEs with BOM sometimes, let's just ignore.

--- scripts/verify-encoding.ps1
+++ scripts/verify-encoding.ps1
@@ -33,7 +33,7 @@ $lineEndingErrors = @()
 try {
     Push-Location $SourceRoot
     foreach ($file in $textFiles) {
-        if ([IO.Path]::GetExtension($file) -eq '.DotSettings') {
+        if (@('.dotsettings', '.resx') -contains [IO.Path]::GetExtension($file).ToLowerInvariant()) {
             continue
         }
 
