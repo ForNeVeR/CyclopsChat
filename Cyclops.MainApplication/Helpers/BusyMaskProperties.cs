d74fa0442bf0774b86e72275ef6d1285dbfe07bf
Friedrich von Never <friedrich@fornever.me>
2025-03-08 23:57:31 +0100

    (#41) Get rid of WPF Extended Tooling, replace with other free controls

---
+++ Cyclops.MainApplication/Helpers/BusyMaskProperties.cs
@@ -0,0 +1,29 @@
+using System.Windows;
+using BusyIndicator;
+
+namespace Cyclops.MainApplication.Helpers;
+
+public static class BusyMaskProperties
+{
+    /// <summary>
+    /// The same as <see cref="BusyMask.IsBusy"/> but with <see cref="BusyMask.IsBusyAtStartup"/> combined, so it works
+    /// properly for the cases when <c>IsBusy</c> was <c>true</c> at the moment of the control creation.
+    /// </summary>
+    public static readonly DependencyProperty IsBusyWorkProperty =
+        DependencyProperty.RegisterAttached(
+            "IsBusyWork",
+            typeof(bool),
+            typeof(BusyMaskProperties),
+            new PropertyMetadata(false, OnIsBusyWorkChanged));
+
+    public static bool GetIsBusyWork(DependencyObject obj) => (bool)obj.GetValue(IsBusyWorkProperty);
+
+    public static void SetIsBusyWork(DependencyObject obj, bool value) => obj.SetValue(IsBusyWorkProperty, value);
+
+    private static void OnIsBusyWorkChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var busyMask = (BusyMask)d;
+        busyMask.IsBusyAtStartup = (bool)e.NewValue;
+        busyMask.IsBusy = (bool)e.NewValue;
+    }
+}
--- THIRD-PARTY-SOFTWARE.md
+++ THIRD-PARTY-SOFTWARE.md
@@ -1,8 +1,9 @@
 Binary distributions of CyclopsChat include the following third-party software:
 
 - .NET Runtime: see the license information in `third-party/dotnet`.
+- BusyIndicators: see the license information in `third-party/BusyIndicators`.
 - CommonServiceLocator: the license information in `third-party/CommonServiceLocator`.
-- Extended.Wpf.Toolkit: see the license information in `third-party/Extended.Wpf.Toolkit`.
+- Dirkster.NumericUpDownLib: see the license information in `third-party/Dirkster.NumericUpDownLib`.
 - MvvmLightLibs: the license information in `third-party/MvvmLightLibs`.
 - SharpXMPP: see the license information in `third-party/SharpXMPP`.
 - Unity: see the license information in `Unity`.
